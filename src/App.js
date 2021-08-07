ab5aff98972a6cfbbab2da27b1efe53f94f1d07d
leebbong <56666728+sblee1031@users.noreply.github.com>
2021-08-07 20:45:47 +0900

    React Repository 생성

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
+import styles from "./App.module.css";
+
+function App() {
+  const name = "Tom";
+  const naver = {
+    name: "네이버",
+    url: "http://naver.com",
+  };
+  return (
+    <div className="App">
+      <Hello />
+      <div className={styles.box}>App</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Hello.js
@@ -0,0 +1,25 @@
+// const Hello = () => {
+//   <p>Hello</p>;
+// };
+// export default Hello;
+
+import World from "./World";
+import styles from "./Hello.module.css";
+
+export default function Hello() {
+  return (
+    <div>
+      <h1
+        style={{
+          color: "#f00",
+          borderRight: "12px solid #000",
+          marginBottom: "50px",
+          opacity: 0.5,
+        }}
+      >
+        Hello
+      </h1>
+      <div className={styles.box}>Hello</div>
+    </div>
+  );
+}
